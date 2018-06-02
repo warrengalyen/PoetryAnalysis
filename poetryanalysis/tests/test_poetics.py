24706813dba51e4143654213357a987139f1ea9c
warrengalyen <wgalyen@gmail.com>
2018-06-02 07:27:32 -0700

    Initial commit

---
+++ poetryanalysis/tests/test_poetics.py
@@ -0,0 +1,45 @@
+import os
+import unittest
+import poetryanalysis
+
+
+class TestPoems(unittest.TestCase):
+    def open_poem(self, poem):
+        with open(os.path.join('poetryanalysis/poems', poem)) as f:
+            return poetryanalysis.tokenize(f.read())
+
+    def test_haiku(self):
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('haiku.txt')) == 'haiku')
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('tanka.txt')) == 'tanka')
+
+    def test_sonnets(self):
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('sonnet.txt')) == 'Shakespearean sonnet')
+        self.assertTrue(poetryanalysis.guess_form(self.open_poem(
+            'brokensonnet.txt')) == 'sonnet with unusual meter')
+
+    def test_blankverse(self):
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('blankverse.txt')) == 'blank verse')
+
+    def test_cinquain(self):
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('cinquain.txt')) == 'cinquain')
+
+    def test_ottavarima(self):
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('ottavarima.txt')) == 'ottava rima')
+
+    def test_heroiccouplets(self):
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('heroiccouplets.txt')) == 'heroic couplets')
+
+    def test_rondeau(self):
+        self.assertTrue(poetryanalysis.guess_form(
+            self.open_poem('rondeau.txt')) == 'rondeau')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='poetryanalysis',
+      version='0.2',
+      description='Analyze rhyme scheme, meter and form of poems',
+      long_description=readme(),
+      author='Warren Galyen',
+      url='http://www.mechanikadesign.com',
+      license='MIT',
+      packages=['poetryanalysis'],
+      install_requires=['python-levenshtein'],
+      include_package_data=True
+)
